f252fd87b489026522d61cd7252b3e9b184815da
lisztli <sockpuppet.lea@gmail.com>
2015-11-23 00:49:06 +0800

    init slides

---
+++ slides/151121.parser/pdf.bat
@@ -0,0 +1,13 @@
+c:\CTEX\MiKTeX\miktex\bin\pdflatex.exe --synctex=1 -shell-escape slide.tex
+del *.aux /s
+del *.bak /s
+del *.log /s
+del *.bbl /s
+del *.blg /s
+del *.thm /s
+del *.toc /s
+del *.out /s
+del *.gz /s
+del *.nav /s
+del *.snm /s
+del *.vrb /s
